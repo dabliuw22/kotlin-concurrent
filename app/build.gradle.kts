47ab8d2d7a1ff0e4b506cf36c4b5aff990e14fed
Will Leyton <wleylop22@gmail.com>
2024-04-27 00:57:33 -0500

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("kotlin.concurrent.kotlin-application-conventions")
     alias(libs.plugins.kotlinter)
+    alias(libs.plugins.ksp)
 }
 
 dependencies {
@@ -8,6 +9,8 @@ dependencies {
     implementation(libs.arrow.kt.core)
     implementation(libs.arrow.kt.coroutines)
     implementation(libs.arrow.kt.stm)
+    implementation(libs.arrow.kt.optics)
+    ksp(libs.arrow.kt.optics.ksp.plugin)
     implementation(project(":kotlin-arrow"))
     implementation(project(":kotlin-coroutines"))
 }
--- kotlin-arrow/build.gradle.kts
+++ kotlin-arrow/build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     id("kotlin.concurrent.kotlin-library-conventions")
     alias(libs.plugins.kotlinter)
+    alias(libs.plugins.ksp)
 }
 
 dependencies {
     implementation(libs.arrow.kt.core)
     implementation(libs.arrow.kt.coroutines)
     implementation(libs.arrow.kt.stm)
+    implementation(libs.arrow.kt.optics)
+    ksp(libs.arrow.kt.optics.ksp.plugin)
 }
